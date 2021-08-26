b631461201c2137cee8f5d0a6d0ed18bb74afabd
AngieMeG <70661112+AngieMeG@users.noreply.github.com>
2021-08-26 13:49:19 -0500

    Anotaciones realizadas

--- src/main/java/edu/eci/arsw/springdemo/SpanishSpellChecker.java
+++ src/main/java/edu/eci/arsw/springdemo/SpanishSpellChecker.java
@@ -1,15 +1,15 @@
 package edu.eci.arsw.springdemo;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
+@Component
+@Qualifier("Spn")
 public class SpanishSpellChecker implements SpellChecker {
 
 	@Override
 	public String checkSpell(String text) {
-		return "revisando ("+text+") con el verificador de sintaxis del espanol";
-                
-                
+		return "revisando ("+text+") con el verificador de sintaxis del espanol";   
 	}
 
 }
